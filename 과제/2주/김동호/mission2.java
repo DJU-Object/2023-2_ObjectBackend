6f99c2fa5b32f39e6e8f9aa700503e05d9286116
K-Dongho <kdongho9501@gmail.com>
2023-09-21 19:43:09 +0900

    2주차 과제

---
+++ 과제/2주/김동호/mission2.java
@@ -0,0 +1,18 @@
+package mission2;
+
+public class mission2 {
+
+    public static void main(String[] args) {
+        Store store = new Store();
+
+        store.name = "가게1";
+        store.item = "상품1";
+        store.totalMoney = 1000;
+
+        System.out.println("store.name = " + store.name);
+        System.out.println("store.item = " + store.item);
+        System.out.println("store.totalMoney = " + store.totalMoney);
+        
+        store.buy();
+    }
+}
